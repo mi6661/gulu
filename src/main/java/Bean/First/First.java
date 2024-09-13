45a87e937984cf58bf675386cc63cac3bb20d603
gulu <gulu24257@gmail.com>
2024-09-13 15:39:10 +0800

    完成了第一种方式的beean的配置

--- src/main/java/Bean/First/First.java
+++ src/main/java/Bean/First/First.java
@@ -1,12 +1,18 @@
-//练习Springboot 中bean的三种注入方式
-
+//1、传统的xml文件注入
 
 package Bean.First;
 
-
 public class First {
 
+    public First(){
+        System.out.println("First:无餐构成");
+    }
+    public First(String s){
+        System.out.println("First:有参构造-"+s);
+    }
 
-
-
+    //方法
+    public void test(){
+        System.out.println("this is a test class");
+    }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import Bean.First.First;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		//有参构造
+		First first = (First)context.getBean("first_full");
+		first.test();
+		//无参构造
+		First first_no = (First)context.getBean("first_no");
+		first_no.test();
 	}
 
 }
