7526383ae9e365544609c71495520a54501ce041
gulu <gulu24257@gmail.com>
2024-09-13 16:10:44 +0800

    完成了第二种的编写

---
+++ src/main/java/Bean/Second/Second.java
@@ -0,0 +1,23 @@
+package Bean.Second;
+
+//第二章bean的配置-通过注解
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;//这就个注释都可以标记bean，只是用途不一样
+
+@Controller("second")
+public class Second {
+    public Second(){
+        System.out.println("使用了有参构造");
+    }
+    public Second(String s){
+        System.out.println("使用了有参构造："+s);
+    }
+
+    public void test(){
+        System.out.println("this is test");
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import Bean.First.First;
+import Bean.Second.Second;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +17,20 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+		//一、第一种
+
 		//有参构造
 		First first = (First)context.getBean("first_full");
 		first.test();
 		//无参构造
 		First first_no = (First)context.getBean("first_no");
 		first_no.test();
+
+		//二、第二种
+
+		Second second = (Second)context.getBean("second");
+		second.test();
 	}
 
 }
